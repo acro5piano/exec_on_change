4230f77a1afee24e0c01b38edbc348269035471e
gosho-kazuya <ketsume0211@gmail.com>
2016-09-18 15:55:41 +0900

    add namespace

--- src/exec_on_change.sh
+++ src/exec_on_change.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-usage(){
+exec_on_change::usage(){
     echo "Usage:"
     echo "    exec_on_change file command"
     exit 1
 }
 
-die(){
+exec_on_change::die(){
     echo $1
     exit 2
 }
 
-clear_console(){
+exec_on_change::clear_console(){
     clear
     echo "exec      : $COMMAND"
     echo "at        : `date`"
@@ -20,25 +20,25 @@ clear_console(){
     printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 }
 
-wait_for_change(){
+exec_on_change::wait_for_change(){
     inotifywait --exclude $IGNORE_FILES -r -e MODIFY "$ON_CHANGE" 2>/dev/null || exit
 }
 
-check_requirements(){
+exec_on_change::check_requirements(){
     [ `which inotifywait` ] || die 'Please install inotify-tools'
 }
 
-check_args(){
+exec_on_change::check_args(){
     [ "$ON_CHANGE" ] || usage
     [ "$COMMAND" ] || usage
     [ -e "$ON_CHANGE" ] || die "No such file or directory: $ON_CHANGE"
 }
 
-start_watch(){
-    clear_console
+exec_on_change::start_watch(){
+    exec_on_change::clear_console
     while true; do
-        wait_for_change
-        clear_console
+        exec_on_change::wait_for_change
+        exec_on_change::clear_console
         $COMMAND
     done
 }
